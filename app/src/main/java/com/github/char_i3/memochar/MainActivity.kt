c463fd485ffd6a0c99c9ba00df34758169176060
char_i3 <snoopy.tikuha@gmail.com>
2019-02-16 23:49:03 +0900

    Twitterボタンのidが取れてトースト焼けたよー

--- app/src/main/java/com/github/char_i3/memochar/MainActivity.kt
+++ app/src/main/java/com/github/char_i3/memochar/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.github.char_i3.memochar
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.widget.ImageButton
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonTwitter = findViewById<ImageButton>(R.id.button_twitter)
+
+        buttonTwitter.setOnClickListener {
+            Toast.makeText(this@MainActivity, "ついった！", Toast.LENGTH_LONG).show()
+            Log.d("char", "ついった！！")
+        }
     }
 }
